6e0af6436af523ec9f49fb3d35f5887c84f53afa
Ana Paulina González Rodríguez <158597437+PaulinaGonzalezRodriguez@users.noreply.github.com>
2024-04-09 17:02:12 +0000

    combinada

---
+++ miPrincipal/DemoEstCombinada.java
@@ -0,0 +1,43 @@
+package miPrincipal;
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.LinkedList;
+public class DemoEstCombinada {
+    public static void menu(){
+        System.out.println("************************");
+        System.out.println("  Estructura combinada  ");
+        System.out.println("HashTable-LindkedList-Vector");
+        System.out.println("************************");
+        System.out.println();
+        HashTable2<Integer> ht= new HashTable2<Integer>();
+
+        ht.put("par", 2);
+        ht.put("par", 4);
+        ht.put("par", 6);
+        ht.put("impar", 1);
+        ht.put("par", 8);
+        ht.put("impar", 3);
+        ht.put("impar", 5);
+        ht.put("par", 10);
+        ht.put("impar", 7);
+        ht.put("impar", 9);
+        
+        LinkedList<Integer>pares = ht.get("par");
+        for (Integer i : pares) {
+            System.out.println(i); 
+        }
+        LinkedList<Integer> impares = ht.get("impar");
+        for (Integer i : impares) {
+            System.out.println(i); 
+        }
+        Collection<String> keys=ht.keys();
+        for (String k : keys) {
+            System.out.println(k);
+            
+        }
+
+    }
+
+
+    
+}
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -4,44 +4,58 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        Scanner consola = new Scanner(System.in);
-        int opc =0;
-        do{
-            System.out.println("*°°°°°°°°°°°°°°°°°°°°°°°°°°");
-            System.out.println("    Estructuras lineales   ");
-            System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°");
-            System.out.println(" 1) Pila");
-            System.out.println(" 2) Stack");
-            System.out.println(" 3) Delimitador");
-      
-            System.out.println();
+      Scanner consola = new Scanner(System.in);
+      int opc =0;
+      do{
+          System.out.println("*************************");
+          System.out.println("  Estructuras Lineales   ");
+          System.out.println("*************************");
+          System.out.println(" 1) Pila");
+          System.out.println(" 2) Stack");
+          System.out.println(" 3) Delimitadores");
+          System.out.println(" 4) Expresion Infija-Prefija");
+          System.out.println(" 5) Cola");
+          System.out.println(" 6) Queue");
+          System.out.println(" 7) Tablas de Dispersion");
+          System.out.println(" 8) Estructura combinada");
 
-            System.out.println(" 0) SALIR");
-            System.out.print("Selecciona opción:");
-            opc=consola.nextInt();
+          System.out.println();
 
-            switch (opc){
-            case 1:
-               DemoPila.menu();
-               break;
-            case 2:
-               DemoStack.menu();
-               break;
-            case 3:
-               DemoDelimitador.menu();
-               break;
-            case 0:
-               System.out.println("ADIOS!");
-               consola.close();
-               break;
-               
+          System.out.println("0) SALIR");
+          System.out.print("Selecciona opción:");
+          opc=consola.nextInt();
+          switch (opc){
+              case 1:
+                 DemoPila.menu();
+                 break;
+              case 2:
+                 DemoStack.menu();
+                 break;
+              case 3:
+                 DemoDelimitador.menu();
+                 break;
+              case 4:
+                 //tarea
+              case 5:
+                 DemoCola.menu();
+                 break;
+              case 6:
+                 DemoQueue.menu();
+                 break;
+              case 7:
+                 DemoTablaDispersion.menu(); break;
+                case 8: 
+                DemoEstCombinada.menu();break;
+              case 0:
+                  System.out.println("ADIOS!");
+                  //consola.close();
+                  break;
+              default:
+                  System.out.println("Valor incorrecto, intente de nuevo!");
+          }
 
-            default:
-               System.out.println("Valor incorrecto, intente de nuevo!");
+      } while (opc != 0);
 
-        }
-       
-    }while (opc != 0);
-		
-    }
+  }
+  
 }
